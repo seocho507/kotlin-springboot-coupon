d291456803510b6f26bb52598e258165327978c0
seocho507 <gram753@naver.com>
2024-10-08 20:31:35 +0900

    feat : consumer 모듈 추가

---
+++ coupon-consumer/src/main/kotlin/com/seocho507/couponconsumer/CouponConsumerApplication.kt
@@ -0,0 +1,11 @@
+package com.seocho507.couponconsumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponConsumerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponConsumerApplication>(*args)
+}
