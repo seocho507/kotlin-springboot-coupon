ce083b4da8996ee38f19a38c5664e264eb81f83e
seocho507 <gram753@naver.com>
2024-10-08 20:30:26 +0900

    feat : api 모듈 추가

---
+++ coupon-api/src/main/kotlin/com/seocho507/couponapi/CouponApiApplication.kt
@@ -0,0 +1,11 @@
+package com.seocho507.couponapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponApiApplication>(*args)
+}
