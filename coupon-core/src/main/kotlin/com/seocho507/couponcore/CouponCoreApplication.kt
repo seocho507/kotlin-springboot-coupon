38d392590a662158c1d2414dc23e8d3080c09618
seocho507 <gram753@naver.com>
2024-10-08 20:29:39 +0900

    feat : core 모듈 추가

---
+++ coupon-core/src/main/kotlin/com/seocho507/couponcore/CouponCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.seocho507.couponcore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouponCoreApplication>(*args)
+}
