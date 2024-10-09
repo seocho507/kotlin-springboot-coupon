b52ebf83f32ee6b2d9033213d9ba8ef9b1553558
seocho507 <gram753@naver.com>
2024-10-09 15:24:57 +0900

    style : code style 적용

--- load-test/test1.py
+++ load-test/test1.py
@@ -2,6 +2,7 @@ from locust import task, FastHttpUser, stats
 
 stats.PERCENTILES_TO_CHART = [0.95, 0.99]
 
+
 class HelloWorld(FastHttpUser):
     connection_timeout = 10.0
     network_timeout = 10.0
